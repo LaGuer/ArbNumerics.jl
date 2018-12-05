b38d09df257bb33ff989535e78ed1e2330ce0f5b
autodocs <autodocs>
2018-12-05 14:34:29 +0000

    build based on f52708c

---
+++ dev/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Overview",
+    "title": "Overview",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#ArbNumerics-1",
+    "page": "Overview",
+    "title": "ArbNumerics",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "references/#",
+    "page": "References",
+    "title": "References",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "references/#References-1",
+    "page": "References",
+    "title": "References",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "documentindex/#",
+    "page": "Index",
+    "title": "Index",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "documentindex/#Index-1",
+    "page": "Index",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+]}
