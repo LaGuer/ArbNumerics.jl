e42b59d2dccfdb135775bd1a64ca8bc2d8183a24
autodocs <autodocs>
2018-12-11 10:18:59 +0000

    build based on 5fe120c

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Overview",
     "title": "ArbNumerics",
     "category": "section",
-    "text": "ArbNumerics.jl offers efficient mathematical computation with multiprecision values\nFloating Point values, Real values (as interval enclosures) and Complex values are supported\nYou can set the precision to a given number of bits (24..8196) or to approximate a number of digits (7..2450)\nMany mathematical functions are available\nSome basic matrix functions are supported\nA Julian perspective on Frederik Johansson\'s Arb C library"
+    "text": "ArbNumerics.jl offers efficient mathematical computation with multiprecision values\nFloating Point values, Real values (as interval enclosures) and Complex values are supported\nSet the precision to a given number of bits (24..8196) or to cover a number of digits (7..2450)\nMany mathematical functions are available\nSome basic matrix functions are supported\nA Julian perspective on Frederik Johansson\'s Arb C library"
 },
 
 {
