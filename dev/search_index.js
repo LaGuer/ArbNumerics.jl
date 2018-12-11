95d3729e187d70a2fcd83a02794bcde397668e35
autodocs <autodocs>
2018-12-11 06:49:36 +0000

    build based on 32842d0

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Overview",
     "title": "ArbNumerics",
     "category": "section",
-    "text": ""
+    "text": "ArbNumerics.jl is a Julian perspective on Frederik Johannson\'s LibArb, a C library offering state of the art computation with multiprecision floating point values and corresponding enclosure intervals."
 },
 
 {
