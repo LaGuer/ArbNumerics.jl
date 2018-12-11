134da64e4d8e77b028c65f3eb123d0065af385d3
autodocs <autodocs>
2018-12-11 08:15:15 +0000

    build based on b44f344

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Overview",
     "title": "ArbNumerics",
     "category": "section",
-    "text": "ArbNumerics.jl is a Julian perspective on Frederik Johansson\'s Arb C library, offering state of the art mathematical computation with multiprecision floating point values and assured interval enclosures. This package outperforms BigFloats when computing elementary and advanced mathematical functions in precisions upto 4000 bits (1200 digits) and beyond.100	0.89 250	0.7 500	0.83 1000	1.07 2000	1.04 3000	1.02 4000	1.02function bits of precision relative speed\n* 100 0.89\n 250 0.7\n 500 0.83\n 1000 1.07\n 2000 1.04\n 3000 1.02\n 4000 1.02"
+    "text": "ArbNumerics.jl is a Julian perspective on Frederik Johansson\'s Arb C library, offering state of the art mathematical computation with multiprecision floating point values and assured interval enclosures. This package outperforms BigFloats when computing elementary and advanced mathematical functions in precisions upto 4000 bits (1200 digits) and beyond.  Performance reflects the Arb C library. Simple arithmetic is somewhat slower than with BigFloat, although polynomial evaluation is somewhat faster than with BigFloat. All the elementary and advanced math functions are faster than with BigFloat."
 },
 
 {
