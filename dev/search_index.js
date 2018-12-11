624080af8856db3cd9e94b006febd95857bca395
autodocs <autodocs>
2018-12-11 07:02:45 +0000

    build based on 5d2557c

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Overview",
     "title": "ArbNumerics",
     "category": "section",
-    "text": "ArbNumerics.jl is a Julian perspective on Frederik Johansson\'s Arb C library, offering state of the art computation with multiprecision floating point values and assured interval enclosures."
+    "text": "ArbNumerics.jl is a Julian perspective on Frederik Johansson\'s Arb C library, offering state of the art computation with multiprecision floating point values and assured interval enclosures. This package outperforms BigFloats for precisions upto 4000 bits (1200 digits) and beyond."
 },
 
 {
